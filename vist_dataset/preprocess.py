1fcdd6594bff4f3a67ad633119cdc384d0df42d5
eunjeeSung <ubuntu@ip-172-31-43-113.us-west-2.compute.internal>
2021-03-11 11:30:49 +0000

    Package preprocessing codes into a class

--- vist_dataset/preprocess.py
+++ vist_dataset/preprocess.py
@@ -1,25 +1,40 @@
 import json
 import argparse
-from collections import defaultdict
 
-def extract_story_info(src_path, dst_path):
-    with open(src_path) as src_file:
-        annotations = json.load(src_file)['annotations']
+import numpy as np
+import nltk
+
+from collections import defaultdict
+from nltk.tokenize import word_tokenize
+
+
+class AnnotationPreprocessing():
+    def __init__(self, src_path):
+        self.src_path = src_path
+        with open(self.src_path) as src_file:
+            self.src_json = json.load(src_file)
+
+    def extract_stories(self):
+        annotations = self.src_json['annotations']
         stories = defaultdict(list)
         for annotation in annotations:
             data = annotation[0]
             story_id, storylet_id, image_id = \
                 data["story_id"], data["storylet_id"], data["photo_flickr_id"]
             stories[story_id].append({"storylet_id": storylet_id, "image_id": image_id})
+        return stories
 
-    with open(dst_path, 'w') as dst_file:
-        json.dump(stories, dst_file)
+    def arr_to_json(self, arr, dst_path):
+        with open(dst_path, 'w') as dst_file:
+            json.dump(arr, dst_file)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--src', action='store', type=str, default='train.story-in-sequence.json')
-    parser.add_argument('--dst', action='store', type=str, default='train.stories.json')
+    parser.add_argument('--src', dest='src_path', type=str, default='train.story-in-sequence.json')
+    parser.add_argument('--dst', dest='dsc_path', type=str, default='train.stories.json')
     args = parser.parse_args()
 
-    extract_story_info(args.src, args.dst)
+    prep = AnnotationPreprocessing(args.src_path)
+    stories = prep.extract_stories()
+    prep.arr_to_json(stories, args.dsc_path)
